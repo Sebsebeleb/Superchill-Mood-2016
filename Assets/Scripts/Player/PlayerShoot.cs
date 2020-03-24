60e526bcc508b1079cdf53573cd5062606269950
Sebastian Vråle <seb@terminator.net>
2020-03-24 14:43:09 +0100

    Player can shoot

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public float FireRate;
+
+    private float nextShootTime;
+
+    [SerializeField] private ParticleSystem flamethrowerParticles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextShootTime)
+        {
+            nextShootTime = Time.time + FireRate;
+
+            flamethrowerParticles.Emit(1);
+        }
+    }
+}
