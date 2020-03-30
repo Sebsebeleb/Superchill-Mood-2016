1ad2881f6aa56502def8bf073571cbf40caa3f14
Skyress-s <mathmoe64@gmail.com>
2020-03-30 12:51:45 +0100

    AI

---
+++ Assets/Scripts/AI/BasicEnemy.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+   public float _hp;
+}
---
+++ Assets/Scripts/AI/BasicFollowPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicFollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _destination;
+
+    private NavMeshAgent _navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+    
+    void SetDestination()
+    {
+        if (_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        SetDestination();
+    }
+}
